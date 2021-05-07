13156c3136bd568a43644c25d5b374f27f93748f
zsu <azure.ss@gmail.com>
2021-05-07 12:49:33 -0400

    Initial check-in of module GenericExporter

---
+++ Example/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using GenericExporter;
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exporter = new Exporter();
+            var items = new List<User>();
+            for(var i=0;i<100;i++)
+                items.Add(new User
+                {
+                    Id = i,
+                    FirstName = $"FirstName{i}",
+                    LastName = $"LastName{i}",
+                    CreatedDate = GetRandomDate(),
+                    Salary = new Random().NextDouble()
+                });
+            var result=exporter.Export<User>(items);
+            File.WriteAllBytes("Example.xslx", result);
+
+        }
+        private static DateTime GetRandomDate()
+        {
+            Random gen = new Random();
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(gen.Next(range));
+        }
+
+    }
+     class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ GenericExporter/Exporter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ClosedXML.Excel;
+
+namespace GenericExporter
+{
+    public class Exporter
+    {
+        public byte[] Export<T>(
+        List<T> rows,
+        ExportType exportType = ExportType.Excel,
+        string[] headers = null,
+        Func<T, object[]> formatterFunc = null)
+        {
+            using (var ms = GetStream(rows, exportType, headers, formatterFunc))
+            {
+                return ms.ToArray();
+            }
+        }
+        public MemoryStream GetStream<T>(
+        List<T> rows,
+        ExportType exportType = ExportType.Excel,
+        string[] headers = null,
+        Func<T, object[]> formatterFunc = null)
+        {
+            var ms = new MemoryStream();
+            switch (exportType)
+            {
+                case ExportType.Excel:
+                    var excel = ToExcel<T>(rows, headers, formatterFunc);
+                    excel.SaveAs(ms);
+                    break;
+            }
+            ms.Position = 0;
+            return ms;
+        }
+        public XLWorkbook ToExcel<T>(
+            List<T> rows,
+            string[] headers = null,
+            Func<T, object[]> formatterFunc = null)
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var row = 1;
+                var col = 1;
+                var worksheet = workbook.Worksheets.Add("Export");
+                var pis = typeof(T).GetProperties();
+                foreach (var h in headers ?? pis.Select(pi => pi.Name).ToArray())
+                {
+                    worksheet.Cell(row, col++).Value = h;
+                }
+                foreach (var o in rows)
+                {
+                    row++;
+                    col = 1;
+                    var values = formatterFunc == null ?
+                        pis.Select(pi => pi.GetValue(o)) : formatterFunc(o);
+                    foreach (var v in values)
+                    {
+                        worksheet.Cell(row, col++).Value = v;
+                    }
+                }
+                worksheet.Columns().AdjustToContents();
+                return workbook;
+            }
+        }
+    }
+    public enum ExportType
+    {
+        Excel
+    }
+}
