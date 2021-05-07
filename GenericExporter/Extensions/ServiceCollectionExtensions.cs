96b628531f8249c56ab42490fe191ec6f2d0bf1c
zsu <azure.ss@gmail.com>
2021-05-07 13:57:51 -0400

    Add dependency injection

---
+++ GenericExporter/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericExporter
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddGenericExporter(this IServiceCollection services)
+        {
+            Register(services);
+            return services;
+        }
+
+        private static void Register(IServiceCollection services)
+        {
+            services.AddTransient<IExporter, Exporter>();
+        }
+    }
+}
